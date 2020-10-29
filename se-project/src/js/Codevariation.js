13de05244c76b28f846bbfa86270ad7632d2a2b9
Yen-Tse Liao <s88037zz@gmail.com>
2020-10-29 13:28:30 +0800

    code variation prototype.

--- se-project/src/js/Codevariation.js
+++ se-project/src/js/Codevariation.js
@@ -36,22 +36,29 @@ class CodeVariation extends React.Component{
             },
           ];
         
-		const chart_width = window.innerWidth * 0.8
-		const chart_height = window.innerHeight * 0.5
+		const chart_width = window.innerWidth * 0.7
+		const chart_height = window.innerHeight * 0.7
 		
         return (
             <Container style={{height:"100%", display:"flex", flexDirection:"row"}}>
                 <Sidenavbar style={{height:"100%"}}/>
 				<Container id="main" style={{height:"100%", display:"flex", flexDirection:"col"}}>
-					<div style={{width:"100%", height:"300px"}}>
-						<h2>{chart_width},{chart_height}</h2>
+					<div style={{width:"100%", height:"100px"}}>
+						<h2>RepositoryName</h2>
 					</div>
 					<div id="chart_region" style={{width:"90%", height:{chart_height}, backgroundColor: "white",
-						marginLeft:window.innerWidth * 0.05, marginRight:window.innerWidth * 0.05}}>
+						marginLeft:window.innerWidth * 0.01, marginRight:window.innerWidth * 0.01}}>
 						<AreaChart width={chart_width} height={chart_height} data={data}>
-							<Area type="monotone" dataKey="code" stroke="#8884d8" fill="#8884d8" />
+                            <defs>
+                                <linearGradient id="colorCode" x1="0" y1="0" x2="0" y2="1">
+                                    <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+                                    <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+                                </linearGradient>
+                            </defs>
+							<Area type="monotone" dataKey="code" stroke="#82ca9d" fullOpacity={1} fill="url(#colorCode)"/>
 							<XAxis dataKey='date'/>
 							<YAxis/> 
+                            <Legend/>
 						</AreaChart>
 					</div> 
 				</Container>
