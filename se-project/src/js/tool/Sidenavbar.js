c7084143e7389559899d9ec4031ba278fbb286c7
Yen-Tse Liao <s88037zz@gmail.com>
2020-12-02 17:18:19 +0800

    [feat] add setting page in Sidenavbar to revise the project name and contecting toekn

--- se-project/src/js/tool/Sidenavbar.js
+++ se-project/src/js/tool/Sidenavbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Nav, Icon,Navbar,Dropdown,IconButton} from 'rsuite';
+import {Nav, Icon,Navbar, Dropdown, IconButton, } from 'rsuite';
 import {Link}  from "react-router-dom";
 class Sidenavbar extends React.Component{
     constructor(props){
@@ -30,38 +30,12 @@ class Sidenavbar extends React.Component{
                     <Link to={"/issue/" + repoName}>
                         <Nav.Item icon={<Icon icon="dashboard" />}>Issue</Nav.Item>
                     </Link>
-                    {/* <Link to="/">
-                        <Nav.Item icon={<Icon icon="user" />}>Log Out</Nav.Item>
-                    </Link> */}
+                    <Link to={ "/setting/" + repoName}>
+                        <Nav.Item icon={<Icon icon="cog" />}>Setting</Nav.Item>
+                    </Link>
                 </Nav>
                 </Navbar.Body>
             </Navbar>
-            // <div style={{ width: 250, height:"auto" }}>
-            //     <Sidenav style={{height:"100%" }}>
-            //         <Sidenav.Body>
-            //             <Nav>
-            //                 <Link to={"/code/" + repoName}>
-            //                     <Nav.Item icon={<Icon icon="dashboard" />}>Code Base</Nav.Item>
-            //                 </Link>
-            //                 <Link to={"/commit/" + repoName}>
-            //                     <Nav.Item icon={<Icon icon="dashboard" />}>Total Commit</Nav.Item>
-            //                 </Link>
-            //                 <Link to={"/committeam/" + repoName}>
-            //                     <Nav.Item icon={<Icon icon="dashboard" />}>Week Commit</Nav.Item>
-            //                 </Link>
-            //                 <Link to={"/issue/" + repoName}>
-            //                     <Nav.Item icon={<Icon icon="dashboard" />}>Issue</Nav.Item>
-            //                 </Link>
-            //                 <Link to="/repository">
-            //                     <Nav.Item icon={<Icon icon="angle-left" />}>Leave Repo</Nav.Item>
-            //                 </Link>
-            //                 <Link to="/">
-            //                     <Nav.Item icon={<Icon icon="user" />}>Log Out</Nav.Item>
-            //                 </Link>
-            //             </Nav>
-            //         </Sidenav.Body>
-            //     </Sidenav>
-            // </div>
         )
     }
 }
