200e579d663d240b4499870df29e704e7878542b
Will <will84568035>
2020-12-11 21:06:53 +0800

    '取得使用者projects'

--- se-project/src/js/Home.js
+++ se-project/src/js/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Table, Column, HeaderCell, Cell } from 'rsuite-table';
-import { Container, Button, Modal, Input } from 'rsuite';
+import { Container, Button, Modal, Input, Icon} from 'rsuite';
 import { Link } from "react-router-dom";
 import MainHeader from './tool/MainHeader'
 import fakeProjectData from '../test_data/fakeProjectData.json'
 import '../css/Home&Repo.css';
+import { requestUserProjects} from './api/projectAPI';
 
 const chart_width = window.innerWidth * 0.7
 const datas = fakeProjectData.projects_data;
@@ -14,6 +15,7 @@ class Home extends React.Component {
         this.state = {
             backdrop: true,
             show: false,
+            datas: undefined
         };
         this.close = this.close.bind(this);
         this.open = this.open.bind(this);
@@ -31,10 +33,58 @@ class Home extends React.Component {
     createProject() {
         this.setState({ show: false });
         alert(`新增Project：(還沒做)`);
+        // addNewProject({ name: "kkkkkkkkkkkkkkk" })
+    }
+
+    //發API取得使用者的所有projects
+    getUserProjects() {
+        requestUserProjects()
+        .then(result => result.data.projects)
+        .then(projects => {
+            console.log('projects', projects)
+            this.setState({ datas: projects})
+        })
+    }
+
+    //projectsTable元件
+    getProjectTable(datas){
+        return (
+            <div className="projectsTable">
+                <Table bordered={true} width={chart_width} height={375} data={datas}>
+                    <Column width={chart_width * 0.6} align="center">
+                        <HeaderCell className="haederCell">Project Name</HeaderCell>
+                        <Cell>
+                            {rowData => {
+                                var pro_path = "/repository/" + rowData.name
+                                return <Link to={pro_path} className="cell">{rowData.name}</Link>
+                            }}
+                        </Cell>
+                    </Column>
+                    <Column width={chart_width * 0.2} align="center">
+                        <HeaderCell className="haederCell">Last Update Time</HeaderCell>
+                        <Cell dataKey="updateTime"></Cell>
+                    </Column>
+                </Table>
+            </div>
+        )
     }
 
     render() {
-        const { backdrop, show } = this.state;
+        const { backdrop, show, datas} = this.state;
+        var projectTable;
+
+        //Table 顯示緩衝設定
+        if (this.state.datas === undefined){
+            this.getUserProjects();
+            projectTable = (
+                <div style={{height:"400px", backgroundColor:"white"}}>
+                    <p style={{ padding: 4, color: '#999', textAlign: 'center',marginTop:"100px"}}>
+                    <Icon icon="spinner" spin /> 請稍後...</p>)
+                </div>
+            )
+        }else projectTable = this.getProjectTable(datas);
+        
+            
         return (
             <Container style={{ backgroundColor: "white" }}>
                 <MainHeader />
@@ -44,25 +94,7 @@ class Home extends React.Component {
                     <Button color="blue" className="creteButton" onClick={this.open}>Create</Button>
                 </div>
 
-
-                <div className="projectsTable">
-                    <Table bordered={true} width={chart_width} height={375} data={datas}>
-                        <Column width={chart_width * 0.6} align="center">
-                            <HeaderCell className="haederCell">Project Name</HeaderCell>
-                            <Cell>
-                                {rowData => {
-                                    var pro_path = "/repository/" + rowData.name
-                                    return <Link to={pro_path} className="cell">{rowData.name}</Link>
-                                }}
-                            </Cell>
-                        </Column>
-                        <Column width={chart_width * 0.2} align="center">
-                            <HeaderCell className="haederCell">Last Update Time</HeaderCell>
-                            <Cell dataKey="updateTime"></Cell>
-                        </Column>
-                    </Table>
-                </div>
-
+                {projectTable}
 
                 <Modal backdrop={backdrop} show={show} onHide={this.close} size="xs">
                     <Modal.Header>
--- se-project/src/js/api/projectAPI.js
+++ se-project/src/js/api/projectAPI.js
@@ -1,6 +1,14 @@
 import { req,Method } from './api'
 
-const project = 'project/'
+const project = 'project'
 export const requestProjectCodeFreq = (data) => {
-    return req(Method.get, project + 'code_freq/' + data.name)
+    return req(Method.get, project + '/code_freq/' + data.name)
 }
+
+export const requestUserProjects = () => {
+    return req(Method.get, project)
+}
+
+// export const addNewProject = (data) => {
+//     return req(Method.post, project +'?name='+ data.name)
+// }
