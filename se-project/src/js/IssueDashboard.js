b43e5dd0aab0040eb41348df4419e71779f61d56
mdvv85009 <mdvv85009@gmail.com>
2020-12-14 21:55:23 +0800

    [Feature] Complete Issue Message Page Function

--- se-project/src/js/IssueDashboard.js
+++ se-project/src/js/IssueDashboard.js
@@ -4,33 +4,69 @@ import Sidenavbar from './tool/Sidenavbar';
 import {Link}  from "react-router-dom";
 import ExpandedTable from './tool/ExpandedTable'
 import MainHeader from './tool/MainHeader'
+import { requestProjectIssueMessage } from './api/projectAPI';
+import fakeIssueMessage from '../test_data/fakeIssueMessage.json'
 
-const data =  [{
-    id: "001", title: "issue_test001", user: "Tony@gmail.com", date:"2020-10-21", discription: "This is test issue 001"
-},{
-    id: "002", title: "issue_test002", user: "Ken@gmail.com", date:"2020-10-22", discription: "This is test issue 002"
-},{
-    id: "003", title: "issue_test003", user: "Bob@gmail.com", date:"2020-11-01", discription: "This is test issue 003"
-},{
-    id: "004", title: "issue_test004", user: "Ancle@gmail.com", date:"2020-10-10", discription: "This is test issue 004"
-}]
+const newDataList = []
 
 class IssueDashboard extends React.Component{
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            proName: this.props.match.params.pro_name,
+            data: undefined,
+        }
+    } 
+    createIssueMessagesTable = () => {
+        if (this.state.data === undefined) {
+            return (<div>loading....</div>)
+        }
+        return (
+                <div style={{marginTop:"30px"}}>
+                    <ExpandedTable data={this.mapJsonToData()} height={window.innerHeight*0.65}></ExpandedTable>
+                </div>
+            )
+    }
+    mapJsonToData = () => {
+        if(newDataList.length == 0){
+            var counter = 1;
+            console.log(this.state)
+            this.state.data.forEach(value => {
+                var newData = {};
+                newData['id'] = counter++;
+                newData['title'] = value.title;
+                newData['date'] = value.time;
+                newData['labels'] = value.labels;
+                newData['comments'] = value.comments;
+                newDataList.push(newData);
+            });
+        }
+        return newDataList
+    }
+    setIssueMessage = (name) => {
+        requestProjectIssueMessage({ name: name })
+            .then(res => res.data)
+            .then(data => {
+                console.log('data', data)
+                this.setState({ data: data.issue })
+                return data.issue
+            })
+    }
+
     render(){
-        var proName = this.props.match.params.pro_name;
+        if(this.state.data === undefined){
+            this.setIssueMessage(this.state.proName)
+        }
         return(
             <Container style={{height:"100%"}}>
                 <MainHeader/>
                 <Container style={{backgroundColor:"white",width:"100%",paddingLeft:"10%", paddingRight:"10%"}}>
                     <Breadcrumb style={{marginBottom:"20px", marginTop:"20px"}}>
                         <Breadcrumb.Item><Link to="/home">Projects</Link></Breadcrumb.Item>
-                        <Breadcrumb.Item active>{proName}</Breadcrumb.Item>
+                        <Breadcrumb.Item active>{this.state.proName}</Breadcrumb.Item>
                     </Breadcrumb>
-                    <Sidenavbar contact={{pro_name:proName}}/>
-                    <div style={{marginTop:"30px"}}>
-                        <ExpandedTable data={data} height={window.innerHeight*0.65}></ExpandedTable>
-                    </div>
+                    <Sidenavbar contact={{pro_name:this.state.proName}}/>
+                    {this.createIssueMessagesTable()}
                 </Container>
             </Container>
         )
--- se-project/src/js/api/projectAPI.js
+++ se-project/src/js/api/projectAPI.js
@@ -2,9 +2,13 @@ import { req } from './api'
 
 const project = 'project'
 export const requestProjectCodeFreq = (data) => {
-    return req.get(project + 'code_freq/' + data.name)
+    return req.get(project + '/code_freq/' + data.name)
 }
 
 export const requestUserProjects = () => {
     return req.get(project)
+}
+
+export const requestProjectIssueMessage = (data) => {
+    return req.get(project + '/issue/' + data.name)
 }
