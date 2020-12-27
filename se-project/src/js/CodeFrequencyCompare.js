a81506c5c413a941a449fe81bda843f04be30b83
s88037zz <s88037zz@gmail.com>
2020-12-27 09:30:17 +0800

    [feat] add comparing code frequency page.js

---
+++ se-project/src/js/CodeFrequencyCompare.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class CodeFrequencyCompare extends Component{
+    render(){
+        return (
+            <div>
+                <h1> Code Frequency Compare</h1>
+            </div>
+        )
+    }
+}
